57242dd4cdc23986ab9a985db9de0be77b28bacf
dslee0525 <lds8835@gmail.com>
2026-01-13 01:31:20 +0900

    feat:위젯 초기 화면 및 공용 컴포넌트 ui 구현

--- src/widgetApp.tsx
+++ src/widgetApp.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styles from "./widget.module.css";
+import VisitorForm from "./components/VisitorForm";
+import { ChatMessage } from "./components/ChatMessage";
 
 export function WidgetApp() {
   console.log("WidgetApp render");
@@ -7,7 +9,7 @@ export function WidgetApp() {
   const [open, setOpen] = useState(false);
   const [available, setAvailable] = useState<boolean>(false);
 
-  const handleClick = () => {
+  const handleClick = async () => {
     setOpen(true);
   };
 
@@ -24,6 +26,32 @@ export function WidgetApp() {
               <img src="/images/ico_close.svg" alt="닫기" />
             </button>
           </div>
+          {open && available && (
+            <div className={styles.widgetContent}>
+              <div className={styles.chatList}>
+                <div className={styles.bubble}>
+                  <ChatMessage
+                    content={`안녕하세요 :)
+                    상담을 시작하기 전에, 간단한 기본 정보를 입력해 주세요.`}
+                    senderType="USER"
+                  />
+                </div>
+                <div className={styles.bubble}>
+                  <VisitorForm />
+                </div>
+              </div>
+            </div>
+          )}
+          {open && available === false && (
+            <div className={styles.widgetNodata}>
+              <img src="/images/ico_message_off.svg" alt="메세지 아이콘" />
+              <p>
+                현재 채팅 위젯 사용이 불가합니다.
+                <br />
+                관리자에게 문의하세요.
+              </p>
+            </div>
+          )}
         </div>
       )}
 
